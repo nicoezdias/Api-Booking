99a626d52691a854c320111dad25bd3782db20d9
nicoezdias <nicolasezdias@gmail.com>
2022-06-16 16:15:41 -0300

    Test cafe

--- src/main/java/com/PI/apiBooking/ProjectApplication.java
+++ src/main/java/com/PI/apiBooking/ProjectApplication.java
@@ -2,12 +2,15 @@ package com.PI.apiBooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "my.property")
 @SpringBootApplication
 public class ProjectApplication {
-
+	private static Integer cafe;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+		System.out.println(cafe);
 	}
 
 }
